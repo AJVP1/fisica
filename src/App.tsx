53198e38dba868299ce3d812a373397ad3d04c74
AJVP1 <armando.charal1998@gmail.com>
2026-03-05 09:04:32 -0500

    inicio del proyecto, ya con react router y tailwind funcionando

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+// importaciones de librerias
+import { HashRouter } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <HashRouter>
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      </HashRouter>
+    </>
+  );
+}
+
+export default App;
