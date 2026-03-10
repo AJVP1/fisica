a7490753afff4df626877f0d6dc64c8aa9985936
AJVP1 <armando.charal1998@gmail.com>
2026-03-09 21:12:59 -0400

    componente de ejercicio1

---
+++ src/pages/Unidad2/E1.tsx
@@ -0,0 +1,19 @@
+import ExerciseLayout from "../../components/Ejecicio";
+
+function Ejercicio1() {
+  return (
+    <ExerciseLayout number={1}>
+      <h3>Enunciado</h3>
+      <p>
+        En una localidad bonaerense, la probabilidad de que en una temporada
+        llueva es de $0,6$. Un productor de soja, cuya función de utilidad es
+        $U(x) = \ln (x)$, obtiene $10.000$ por su cosecha cuando es una
+        temporada de lluvias y $2.000$ cuando no lo es.
+      </p>
+
+      <h3>Solución</h3>
+    </ExerciseLayout>
+  );
+}
+
+export default Ejercicio1;
