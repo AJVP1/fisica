e21b83de767c7966f411a2604313c215afc589b1
AJVP1 <armando.charal1998@gmail.com>
2026-03-24 16:00:32 -0400

    router con enlaces a los ejercicios

--- src/routes/Router4.tsx
+++ src/routes/Router4.tsx
@@ -3,6 +3,13 @@ import { useParams } from "react-router-dom";
 
 const ejerciciosMap = {
   "ejercicio-1": lazy(() => import("../pages/Unidad4/E1.tsx")),
+  "ejercicio-2": lazy(() => import("../pages/Unidad4/E2.tsx")),
+  "ejercicio-3": lazy(() => import("../pages/Unidad4/E3.tsx")),
+  "ejercicio-4": lazy(() => import("../pages/Unidad4/E4.tsx")),
+  "ejercicio-5": lazy(() => import("../pages/Unidad4/E5.tsx")),
+  "ejercicio-6": lazy(() => import("../pages/Unidad4/E6.tsx")),
+  "ejercicio-7": lazy(() => import("../pages/Unidad4/E7.tsx")),
+  "ejercicio-8": lazy(() => import("../pages/Unidad4/E8.tsx")),
 };
 
 function Router4() {
