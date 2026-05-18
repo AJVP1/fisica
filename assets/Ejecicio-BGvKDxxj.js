af1148d484b08e9a3c83acc3c374610e35635d82
AJVP1 <armando.charal1998@gmail.com>
2026-05-18 13:39:15 -0400

    Updates

--- assets/Ejecicio-BGvKDxxj.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-Cxe0qC43.js";import{M as a}from"./MathJax-CDjAR5JA.js";function c({number:e,children:s}){return t.jsx(a,{children:t.jsxs("section",{className:"bg-[#fafafa] mx-auto p-6",children:[t.jsx("h2",{className:"text-4xl text-center text-[#1d2554] mt-6",children:`Ejercicio ${e}`}),s]})})}export{c as E};
