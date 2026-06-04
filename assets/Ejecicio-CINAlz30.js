a9c75a4b2edcbfc4c9e8af33085ef51540e30cbd
AJVP1 <armando.charal1998@gmail.com>
2026-06-04 16:38:47 -0400

    Updates

--- assets/Ejecicio-CINAlz30.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-DCex1evc.js";import{M as a}from"./MathJax-CCe9E3LZ.js";function c({number:e,children:s}){return t.jsx(a,{children:t.jsxs("section",{className:"bg-[#fafafa] mx-auto p-6",children:[t.jsx("h2",{className:"text-4xl text-center text-[#1d2554] mt-6",children:`Ejercicio ${e}`}),s]})})}export{c as E};
